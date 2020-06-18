29beeff11e216f0d6d8e6c78d4ce3c8d2c06ca3e
mantas <mantasruigys101@gmail.com>
2020-06-18 16:25:23 +0100

    first working build

--- Curling Game/Assets/curlingStone.cs
+++ Curling Game/Assets/curlingStone.cs
@@ -20,8 +20,30 @@ public class curlingStone : NetworkBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if(GetComponent<Rigidbody2D>().velocity.magnitude < 2f) {
-            CmdNotShot();
+        if (isShot) {
+            if (GetComponent<Rigidbody2D>().velocity.magnitude < 2f) {
+               CmdNotShot();
+            }
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (isServer) {
+            if (isShot && collision.gameObject.tag == "playerobj") {
+                Debug.Log("kill" + collision.gameObject.name.ToString());
+
+                collision.gameObject.GetComponent<PlayerController>().isDead = true;
+                collision.gameObject.GetComponent<PlayerController>().hasBall = false;
+
+                collision.gameObject.GetComponent<PlayerController>().swingStone.GetComponent<SpriteRenderer>().enabled = false;
+                collision.gameObject.GetComponent<PlayerController>().gameObject.GetComponent<SpriteRenderer>().enabled = false;
+                collision.gameObject.GetComponent<PlayerController>().broom.GetComponent<SpriteRenderer>().enabled = false;
+
+                GameManage.players[collision.gameObject.GetComponent<PlayerController>().IDName].RpcDie();
+                GameManage.players[owner.GetComponent<PlayerController>().IDName].kills++;
+
+            }
         }
     }
 
