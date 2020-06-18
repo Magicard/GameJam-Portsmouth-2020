19c7b3490f65c7d92af540644b5b5b78b2ee2d14
Teodor Sava <mice.graff@gmail.com>
2020-06-18 22:15:31 +0100

    stuff

--- Curling Game/Assets/Scripts/soundManager.cs
+++ Curling Game/Assets/Scripts/soundManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class soundManager : MonoBehaviour
 {
+    public GameObject player;
+    public AudioSource sweep;
+    public AudioSource shoot;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,23 @@ public class soundManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(1))
+        {
+            if (!sweep.isPlaying)
+            {
+                sweep.Play();
+
+            }
+           
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            shoot.Play();
+            if (gameObject.GetComponent<PlayerController>().hasBall == true)
+            {
+                Debug.Log("shouldplay");
+                
+            }
+        }
     }
 }
